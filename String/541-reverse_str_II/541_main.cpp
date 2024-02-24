1e8957f5013aaa2c3aecc38f68acf82950169c94
charles <1592276395@qq.com>
2024-02-24 16:07:04 +0800

    直接调用algorithm库的reverse函数，简化代码

--- String/541-reverse_str_II/541_main.cpp
+++ String/541-reverse_str_II/541_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -22,42 +23,18 @@ using namespace std;
 class Solution
 {
 public:
-    void reverseString(string &s, int lp, int rp)
-    {
-        while (lp <= rp)
-            swap(s[lp++], s[rp--]);
-    }
-
     string reverseStr(string s, int k)
     {
-        int dk = 2 * k;
-        int p = 0; // 标记当前字符的位置
-        int lp, rp;
-        int size = s.size();
-        while (p < size)
+        for (int i = 0; i < s.size(); i += 2 * k)
         {
-            if (size - p < 2 * k)
-            {
-                if (size - p < k)
-                {
-                    // 如果剩余字符少于k个，将它们全部反转
-                    reverseString(s, p, size - 1);
-                    return s;
-                }
-                else
-                {
-                    // 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样
-                    reverseString(s, p, p + k - 1);
-                    return s;
-                }
-            }
+            if (i + k - 1 < s.size())
+                // 之后的字符数>=k，反转k个
+                reverse(s.begin() + i, s.begin() + i + k); 
             else
-            {
-                reverseString(s, p, p + k - 1);
-                p += 2 * k;
-            }
+                // 之后的字符数不足k，全部反转
+                reverse(s.begin() + i, s.end()); 
         }
-
+         
         return s;
     }
 };
