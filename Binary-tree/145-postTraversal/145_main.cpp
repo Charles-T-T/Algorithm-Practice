6e65d823a86d6aa02eaf3877ba4e6e6d07b8836b
charles <1592276395@qq.com>
2024-03-06 22:31:36 +0800

    Q145 solved

---
+++ Binary-tree/145-postTraversal/145_main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+
+示例 1：
+输入：root = [1,null,2,3]
+输出：[1,2,3]
+
+示例 2：
+输入：root = []
+输出：[]
+
+示例 3：
+输入：root = [1]
+输出：[1]
+
+示例 4：
+输入：root = [1,2]
+输出：[1,2]
+
+示例 5：
+输入：root = [1,null,2]
+输出：[1,2]
+*/
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+private:
+    vector<int> resVec;
+
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        if (root == nullptr)
+            return resVec;
+        
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        resVec.push_back(root->val);
+
+        return resVec;
+    }
+};
