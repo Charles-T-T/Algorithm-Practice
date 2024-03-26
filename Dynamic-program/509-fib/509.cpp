b7b38aef2311aafedff8c2c33700985edd813e1f
charles <1592276395@qq.com>
2024-03-26 12:46:45 +0800

    5-斐波那契数列：动态规划标准方法解决

--- Dynamic-program/509-fib/509.cpp
+++ Dynamic-program/509-fib/509.cpp
@@ -37,4 +37,18 @@ public:
         else
             return fib(n - 1) + fib(n - 2);
     }
+
+    int fib_dynamic(int n)
+    {
+        if (n <= 1)
+            return n;
+        int dp[n + 1];
+        dp[0] = 0, dp[1] = 1;
+        for (int i = 2; i < n + 1; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 };
