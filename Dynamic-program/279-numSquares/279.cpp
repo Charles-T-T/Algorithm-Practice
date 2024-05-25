627513e793a6cd2c83384d12f001a00c5636b979
charles <1592276395@qq.com>
2024-05-25 23:41:25 +0800

    Q279-完全平方数：动态规划解决

---
+++ Dynamic-program/279-numSquares/279.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+
+完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+
+示例 1：
+输入：n = 12
+输出：3 
+解释：12 = 4 + 4 + 4
+
+示例 2：
+输入：n = 13
+输出：2
+解释：13 = 4 + 9
+*/
+
+class Solution {
+public:
+    int numSquares(int n) {
+        /**
+         * 动态规划
+         * dp[i]为表示i所需的完全平方数的最少数量
+         * 显然，这些完全平方数的范围是[1, sqrt(i)]
+         * 假设遍历到其中一个数为j，那么还需要i - j^2
+         * 此时dp[i] = 1 + min(dp[i - j^2])
+        */ 
+        if (n == 1)
+            return 1;
+
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i){
+            int curMin = 1e9;
+            for (int j = 1; j * j <= i; ++j){
+                curMin = min(curMin, dp[i - j * j]); 
+            }
+            dp[i] = curMin + 1;
+        }
+        return dp[n];
+    }
+};
+
+int main(){
+    int n = 12;
+    Solution obj;
+    cout << "res: " << obj.numSquares(n) << endl;
+
+    return 0;
+}
