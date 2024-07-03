072a6688332ced589fbead85cd1b994cdcdacc23
fanhaotian <1592276395@qq.com>
2024-07-03 12:59:08 +0800

    Q27-移除元素：暑假刷题day1，二刷，添加笔记

--- Array/27-remove_the_value/27_main.cpp
+++ Array/27-remove_the_value/27_main.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 /*
-����һ������ nums ��һ��ֵ val������Ҫ ԭ�� �Ƴ�������ֵ���� val ��Ԫ�أ��������Ƴ���������³��ȡ�
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
 
-��Ҫʹ�ö��������ռ䣬������ʹ�� O(1) ����ռ䲢 ԭ�� �޸��������顣
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
 
-Ԫ�ص�˳����Ըı䡣�㲻��Ҫ���������г����³��Ⱥ����Ԫ�ء�
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
 
-ʾ�� 1��
+示例 1：
 
-���룺nums = [3,2,2,3], val = 3
-�����2, nums = [2,2]
-���ͣ�����Ӧ�÷����µĳ��� 2, ���� nums �е�ǰ����Ԫ�ؾ�Ϊ 2���㲻��Ҫ���������г����³��Ⱥ����Ԫ�ء����磬�������ص��³���Ϊ 2 ���� nums = [2,2,3,3] �� nums = [2,2,0,0]��Ҳ�ᱻ������ȷ�𰸡�
-ʾ�� 2��
+输入：nums = [3,2,2,3], val = 3
+输出：2, nums = [2,2]
+解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+示例 2：
 
-���룺nums = [0,1,2,2,3,0,4,2], val = 2
-�����5, nums = [0,1,3,0,4]
-���ͣ�����Ӧ�÷����µĳ��� 5, ���� nums �е�ǰ���Ԫ��Ϊ 0, 1, 3, 0, 4��ע�������Ԫ�ؿ�Ϊ����˳���㲻��Ҫ���������г����³��Ⱥ����Ԫ�ء�
+输入：nums = [0,1,2,2,3,0,4,2], val = 2
+输出：5, nums = [0,1,3,0,4]
+解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
 */
 
 class Solution
@@ -26,7 +26,7 @@ class Solution
 public:
     int removeElement1(vector<int> &nums, int val)
     {
-        // ˼·������Ҫɾ���ģ��ͺ���������ǰ���루����˳�����ɾ������ĩβԪ��pop_back()��
+        // 思路：遇到要删除的，就后面依次向前补齐（类似顺序表的删除），末尾元素pop_back()掉
         int initSize = nums.size(), i = 0;
         while (i < nums.size())
         {
@@ -46,45 +46,14 @@ public:
 
     int removeElement2(vector<int> &nums, int val)
     {
-        // ˼·��˫ָ�룬���ָ�������ָ��֮ǰ�ƶ�
-        int test = 0, fill = 0;
-        while (test < nums.size())
+        // 思路：双指针，检测指针（快）在填充指针（慢）之前移动
+        int fast = 0, slow = 0;
+        while (fast < nums.size())
         {
-            if (nums[test] != val)
-            {
-                nums[fill] = nums[test];
-                fill++;
-            }
-            test++;
+            if (nums[fast] != val)
+                nums[slow++] = nums[fast];
+            fast++;
         }
-        return fill;
+        return slow; // 显然此时的slow恰好就是nums中不等于val的元素的数量
     }
 };
-
-int main(){
-    vector<int> nums;
-    int val, size, num;
-    cout << "please input the val: ";
-    cin >> val;
-    cout << "please input the size of nums: ";
-    cin >> size;
-    cout << "please input the nums, divided by space: ";
-    int i = 0;
-    while (i < size)
-    {
-        cin >> num;
-        nums.push_back(num);
-        i++;
-    }
-
-    Solution solution;
-    cout << "new size of nums: " << solution.removeElement2(nums, val) << endl;
-    cout << "new nums: [ ";
-    for (int i = 0; i < nums.size(); i++)
-    {
-        cout << nums[i] << " ";
-    }
-    cout << "]" << endl;
-
-    return 0;
-}
