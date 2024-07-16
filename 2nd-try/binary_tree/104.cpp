cf324d57a39f25d006810046a983a002944491f1
fanhaotian <1592276395@qq.com>
2024-07-16 22:21:39 +0800

    几道简单二叉树题目，先用递归速通

---
+++ 2nd-try/binary_tree/104.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    // 递归法
+    int maxDepth(TreeNode *root)
+    {
+        if (!root) 
+            return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
