2acb0b2f31276943af733322a7b31053c75578a5
fanhaotian <1592276395@qq.com>
2025-07-02 23:41:43 +0800

    994.腐烂的橘子：多源BFS解决，速度提升

--- Graph-theory/994-orangesRotting/994.cpp
+++ Graph-theory/994-orangesRotting/994.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <thread>
+#include <queue>
 #include <unordered_set>
 #include <utility>
 #include <vector>
@@ -61,10 +61,64 @@ class Solution {
   }
 };
 
-int main() {
-  vector<vector<int>> grid = {{0, 1, 1}, {1, 1, 1}};
+// LeetCode官方题解：多源BFS
+class Solution_2 {
+ public:
+  int orangesRotting(vector<vector<int>>& grid) {
+    vector<int> dir_x = {-1, 0, 1, 0};
+    vector<int> dir_y = {0, -1, 0, 1};
+    int fresh_cnt = 0;                // 剩余的新鲜橘子数目
+    queue<pair<int, int>> rotten_qu;  // 已经被腐蚀的橘子的坐标
 
-  Solution obj;
+    int row = grid.size();
+    int col = grid[0].size();
+    vector<vector<int>> turn(row, vector<int>(col, -1));  // 被腐蚀所需轮数
+
+    int res = 0;
+
+    for (int i = 0; i < row; ++i) {
+      for (int j = 0; j < col; ++j) {
+        if (grid[i][j] == 2) {
+          turn[i][j] = 0;  // 一开始就是被腐蚀的，对应0轮就被腐蚀
+          rotten_qu.emplace(i, j);
+        } else if (grid[i][j] == 1) {
+          ++fresh_cnt;
+        }
+      }
+    }
+
+    while (!rotten_qu.empty()) {
+      auto [x, y] = rotten_qu.front();
+      rotten_qu.pop();
+
+      // 探索它上下左右四个方向有没有可以被腐蚀的
+      for (int i = 0; i < 4; ++i) {
+        int cx = x + dir_x[i];
+        int cy = y + dir_y[i];
+        if (cx < 0 || cx >= row || cy < 0 || cy >= col ||  // 越界
+            turn[cx][cy] != -1 ||                          // 已经被腐蚀过了
+            !grid[cx][cy]                                  // 空格子
+        ) {
+          continue;
+        }
+
+        turn[cx][cy] = turn[x][y] + 1;
+        res = turn[cx][cy];
+        --fresh_cnt;
+        if (!fresh_cnt) break;
+        rotten_qu.emplace(cx, cy);
+      }
+      if (!fresh_cnt) break;
+    }
+
+    return fresh_cnt ? -1 : res;
+  }
+};
+
+int main() {
+  vector<vector<int>> grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+
+  Solution_2 obj;
   auto res = obj.orangesRotting(grid);
   cout << res << endl;
 
