36bb19c38b24c28bad2d76b1c626e7e346015fc3
fanhaotian <1592276395@qq.com>
2025-08-20 18:41:54 +0800

    pass 77.组合 in golang

--- Back-tracking/77-combination/go/77.go
+++ Back-tracking/77-combination/go/77.go
@@ -1,36 +1,43 @@
 package main
 
-import "fmt"
+import	"fmt"
 
-type Helper struct {
-	path []int
-	res  [][]int
+var	result [][]int
+var path []int
+
+func appendCopy(result [][]int, path []int) [][]int {
+	tmp := make([]int, len(path))
+	copy(tmp, path)
+	return append(result, tmp)
 }
 
-func (helper *Helper) backTracking(start int, end int, k int) {
-	// 递归出口
-	if len(helper.path) == k {
-		// newPath := make([]int, len(helper.path))
-		// copy(newPath, helper.path)
-		// helper.res = append(helper.res, newPath)
-		helper.res = append(helper.res, append([]int(nil), helper.path...))
+func backTracking(n int, start int, k int) {
+	if len(path) == k {
+		result = appendCopy(result, path)
 		return
 	}
-	// 横向遍历
-	for i := start; i <= end; i++ {
-		helper.path = append(helper.path, i)           // 处理
-		helper.backTracking(i+1, end, k)               // 递归
-		helper.path = helper.path[:len(helper.path)-1] // 回溯
-	}
+
+	for i := start; i <= n; i++ {
+		path = append(path, i)
+		backTracking(n, i + 1, k)
+		path = path[:len(path)-1]
+	} 
 }
 
 func combine(n int, k int) [][]int {
-	helper := Helper{}
-	helper.backTracking(1, n, k)
-	return helper.res
+	result = make([][]int, 0)
+	path = make([]int, 0)
+	backTracking(n, 1, k)
+
+	return result
 }
 
 func main() {
-	res := combine(4, 2)
-	fmt.Println(res)
+	var n int
+	var k int
+	fmt.Printf("Please input n and k: ")
+	fmt.Scanf("%d %d", &n, &k)
+	fmt.Println("n, k: ", n, k)
+	combine(n, k)
+	fmt.Println(result)
 }
--- String/28-strStr/28.cpp
+++ String/28-strStr/28.cpp
@@ -4,46 +4,89 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    // KMP，启动！
-    vector<int> getNextArr(const string &needle) {
-        vector<int> next(needle.size(), 0);
-        for (int i = 0, j = 1; j < next.size(); j++) {
-            // 不匹配：i回退
-            while (i > 0 && needle[i] != needle[j]) 
-                i = next[i - 1];
-            if (needle[i] == needle[j])
-                i++;
-            next[j] = i;
-        }
-        return next;
+class Solution {
+ public:
+  // KMP，启动！
+  vector<int> getNextArr(const string& needle) {
+    vector<int> next(needle.size(), 0);
+    for (int i = 0, j = 1; j < next.size(); j++) {
+      // 不匹配：i回退
+      while (i > 0 && needle[i] != needle[j]) i = next[i - 1];
+      if (needle[i] == needle[j]) i++;
+      next[j] = i;
     }
+    return next;
+  }
 
-    int strStr(string haystack, string needle)
-    {
-        vector<int> next = getNextArr(needle);
-        for (int i = 0, j = 0; i < haystack.size(); i++) {
-            while (j > 0 && haystack[i] != needle[j]) 
-                j = next[j - 1];
-            if (haystack[i] == needle[j]) 
-                j++;
-            if (j == needle.size())
-                return i - needle.size() + 1;
-        }
-        return -1;
+  int strStr(string haystack, string needle) {
+    vector<int> next = getNextArr(needle);
+    for (int i = 0, j = 0; i < haystack.size(); i++) {
+      while (j > 0 && haystack[i] != needle[j]) j = next[j - 1];
+      if (haystack[i] == needle[j]) j++;
+      if (j == needle.size()) return i - needle.size() + 1;
     }
+    return -1;
+  }
 };
 
+class Solution_2 {
+ public:
+  void genNextArr(int* nextval, string pattern) {
+    int len = pattern.size();
+    int* next = new int[len];
+    int j = 0;
+    next[0] = 0;
+
+    for (int i = 1; i < len; ++i) {
+      while (j > 0 && pattern[i] != pattern[j]) {
+        j = next[j - 1];
+      }
+      if (pattern[i] == pattern[j]) ++j;
+      next[i] = j;
+    }
+
+    nextval[0] = 0;
+    for (int i = 1; i < len; ++i) {
+      if (next[i] == 0) {
+        nextval[i] = 0;
+      } else if (pattern[i] == pattern[next[i] - 1]) {
+        nextval[i] = nextval[next[i] - 1];
+      } else {
+        nextval[i] = next[i];
+      }
+    }
+
+    delete[] next;
+  }
+
+  int strStr(string haystack, string needle) {
+    int len_text = haystack.size();
+    int len_pattern = needle.size();
+    int* next = new int[len_pattern];
+    genNextArr(next, needle);
+
+    int j = 0;
+    for (int i = 0; i < len_text; ++i) {
+      while (j > 0 && haystack[i] != needle[j]) {
+        j = next[j - 1];
+      }
+      if (haystack[i] == needle[j]) ++j;
+      if (j == len_pattern) return i - len_pattern + 1;
+    }
+
+    return -1;
+  }
+};
+
+/*
+a a a a b
+0 1 2 3 0
+
+*/
+
 int main() {
-    string needle = "aabaabaaf"; // 0 1 0 1 2 3 4 5 0
-    Solution obj;
-    vector<int> next = obj.getNextArr(needle);
-    for (int num : next) 
-        cout << num << " ";
-    cout << endl;
+  Solution_2 obj;
+  cout << obj.strStr("mississippi", "issip") << endl;
 
-    return 0;
-
+  return 0;
 }
